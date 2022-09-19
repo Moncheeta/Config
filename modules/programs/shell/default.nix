8a292f960fbb95f8e53bdcff7feb85d83d327545
Moncheeta <monkey.damianek@gmail.com>
2022-09-19 18:26:08 -0500

    Added starship

--- modules/programs/shell/default.nix
+++ modules/programs/shell/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./fish.nix
+    ./prompt.nix
   ];
 }
---
+++ modules/programs/shell/prompt.nix
@@ -0,0 +1,40 @@
+{ ... }:
+
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+
+      directory.style = "blue";
+
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+
+      git_metrics = {
+        format = "[+$added](green)/[-$deleted](red) ";
+      };
+
+      format = "$username\$hostname\$directory\$git_branch\$git_status\$git_metrics\$line_break\$character";
+    };
+  };
+}

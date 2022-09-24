50494f653371f7943eaec0d36c98f5bafc733184
Moncheeta <monkey.damianek@gmail.com>
2022-09-24 16:02:45 -0500

    Added gh extensions

--- modules/programs/github/gh.nix
+++ modules/programs/github/gh.nix
@@ -6,14 +6,14 @@
     enableGitCredentialHelper = true;
     settings.editor = "nvim";
     extensions = with pkgs; [
-      gh-notify
+      #gh-notify
       gh-dash
-      gh-f
-      gh-s
+      #gh-f
+      #gh-s
       gh-eco
-      gh-install
-      gh-branch
-      gh-screensaver
-    ]
+      #gh-install
+      #gh-branch
+      #gh-screensaver
+    ];
   };
 }

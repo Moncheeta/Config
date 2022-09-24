0fb555e98fb56c842afd4d9ed32298cbadc5cff0
Moncheeta <monkey.damianek@gmail.com>
2022-09-24 15:31:11 -0500

    Fixed env vars

--- home.nix
+++ home.nix
@@ -12,24 +12,24 @@
     homeDirectory = "/home/moncheeta";
 
     sessionVariables = {
-      "EDITOR" = "nvim";
-      "VISUAL" = "nvim";
-      "TERMINAL" = "kitty";
-      "KITTY_ENABLE_WAYLAND" = "1";
-      "_JAVA_AWT_WM_NONREPARENTING" = "1";
-      "XCURSOR_SIZE" = "24";
-      "LIBVA_DRIVER_NAME" = "nvidia";
-      "XDG_SESSION_TYPE" = "wayland";
-      "GBM_BACKEND" = "nvidia-drm";
-      "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
-      "WLR_NO_HARDWARE_CURSORS" = "1";
-      "NIXOS_CONFIG_DIR" = "$HOME/Configs";
-      "XDG_CACHE_HOME"   = "$HOME/.cache";
-      "XDG_CONFIG_HOME"  = "$HOME/.config";
-      "XDG_BIN_HOME"     = "$HOME/.local/bin";
-      "XDG_DATA_HOME"   = "$HOME/.local/share";
-      "STEAM_EXTRA_COMPAT_TOOLS_PATHS" = "$HOME/.steam/root/compatibilitytools.d";
-      "PATH" = "$XDG_BIN_HOME:$PATH";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      TERMINAL = "kitty";
+      KITTY_ENABLE_WAYLAND = "1";
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+      XCURSOR_SIZE = "24";
+      LIBVA_DRIVER_NAME = "nvidia";
+      XDG_SESSION_TYPE = "wayland";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NIXOS_CONFIG_DIR = "$HOME/Config";
+      XDG_CACHE_HOME   = "$HOME/.cache";
+      XDG_CONFIG_HOME  = "$HOME/.config";
+      XDG_BIN_HOME     = "$HOME/.local/bin";
+      XDG_DATA_HOME    = "$HOME/.local/share";
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
+      PATH = "$XDG_BIN_HOME:$PATH";
     };
 
     packages = with pkgs; [

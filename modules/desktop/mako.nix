6723f674315c1f25fb08ed448ccfbfd88dab330b
Moncheeta <monkey.damianek@gmail.com>
2022-09-24 18:22:02 -0500

    Added mako

---
+++ modules/desktop/mako.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+  programs.mako = {
+    enable = true;
+    borderRadius = 10;
+    defaultTimeout = 5;
+    font = "JetBrainsMono Nerd Font 10";
+  };
+}
--- modules/home.nix
+++ modules/home.nix
@@ -5,5 +5,6 @@
     ./programs
     ./services
     ./desktop/hyprland.nix
+    ./desktop/mako.nix
   ];
 }

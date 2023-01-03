301091b21037260eb97478ea56a955f70bb2fbf8
Moncheeta <moncheeta@duck.com>
2023-01-03 14:33:26 -0600

    Moved around urxvt files

--- Custom/apps/urxvt/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-doas cp ../.Xresources ~
-doas mkdir ~/.urxvt
-doas mkdir ~/.urxvt/ext
-doas cp -r ./urxvt-perls/keyboard-select ./urxvt-perls/deprecated/clipboard ./urxvt-perls/deprecated/url-select ~/.urxvt/ext

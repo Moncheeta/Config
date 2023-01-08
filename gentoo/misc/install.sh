248e44736e17fe90e1fcc3a5c6afaf8bc353b6ae
Moncheeta <moncheeta@duck.com>
2023-01-08 13:39:37 -0600

    Update install scripts

--- gentoo/misc/install.sh
+++ gentoo/misc/install.sh
@@ -6,26 +6,27 @@ CONFIG_DIR="/home/$USER/config"
 KERNEL_CONFIG="$CONFIG_DIR/gentoo/desktop-linux-6.1.2.config" # MUST SET!!!
 
 # Portage
-cp -r $CONFIG_DIR/gentoo/portage/* /etc/portage
+#cp -r $CONFIG_DIR/gentoo/portage/* /etc/portage # expected to already have done
 emerge app-portage/cpuid2cpuflags
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/cpu
-mirrorselect -4Dos 5 -c USA >> /etc/portage/make.conf
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge --verbose --update --deep --newuse @world
 
 # Kernel
 eselect kernel set 1 # select gentoo-sources
 cp $KERNEL_CONFIG /usr/src/linux/.config
-cd /usr/src/linux &&
-\ make -j6 &&
-\ modules_install &&
-\ make install && cd
-emerge osprober
+cd /usr/src/linux
+make -j6
+make modules_install
+make install
+cd
+emerge os-prober
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Programs
-emerge exa zsh app-shells/dash xorg-server alsa-utils htop layman pass doas ufw
+emerge exa zsh xorg-server alsa-utils htop layman pass doas
+\ ufw dhcpcd wpa_supplicant
 \ fzf rxvt-unicode neovim rust clangd black github-cli mcfly
 \ mpv qutebrowser cmus tty-clock # system, dev, and applications
 layman -a guru # many programs(zoxide)
@@ -33,16 +34,17 @@ layman -a vaacus # jedi-language-server
 layman -a jjakob # gomuks
 layman -a robertgzr # shfmt
 layman -a ambasta # stylua
-emerge pfetch zoxide bash-language-server lua-language-server gomuks stylua
-cargo install himalaya
-cd $CONFIG_DIR/xorg/wm/dmenu && make install &&
-    \ cd ../dwm && make install &&
-    \ cd ../slstatus && make install &&
-    \ cd ../../sx && make install &&
-    \ cd ../../terminal/rover && make install &&
-cd $CONFIG_DIR/other/nerd-fonts && ./install.sh
+emerge pfetch zoxide bash-language-server lua-language-server gomuks StyLua
+cd $CONFIG_DIR/xorg/wm/dmenu && make install
+cd ../dwm && make install
+cd ../slstatus && make install
+cd ../../sx && make install
+cd ../../terminal/rover && make install
+cd
 
 # Services
+rc-update add dhcpcd default
+rc-update add wpa_supplicant default
 rc-update add ufw default
 
 # Configs
---
+++ gentoo/misc/install_misc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# DO NOT RUN AS ROOT
+CONFIG_DIR="/home/$USER/config"
+cd $CONFIG_DIR/other/nerd-fonts && ./install.sh && cd
+cargo install himalaya

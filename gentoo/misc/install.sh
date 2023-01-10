2a5f7a3aac17e7f7a0032e88d619cd0bf39d20e4
Moncheeta <moncheeta@duck.com>
2023-01-10 17:17:34 -0600

    Updated portage configuration files

--- gentoo/misc/install.sh
+++ gentoo/misc/install.sh
@@ -6,35 +6,25 @@ CONFIG_DIR="/home/$USER/config"
 KERNEL_CONFIG="$CONFIG_DIR/gentoo/desktop-linux-6.1.2.config" # MUST SET!!!
 
 # Portage
-#cp -r $CONFIG_DIR/gentoo/portage/* /etc/portage # expected to already have done
+cp -r $CONFIG_DIR/gentoo/portage/* /etc/portage # expected to already have done
 emerge app-portage/cpuid2cpuflags
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/cpu
-echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge --verbose --update --deep --newuse @world
 
 # Kernel
 eselect kernel set 1 # select gentoo-sources
 cp $KERNEL_CONFIG /usr/src/linux/.config
-cd /usr/src/linux
-make -j6
-make modules_install
-make install
-cd
 emerge os-prober
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Programs
-emerge exa zsh xorg-server alsa-utils htop layman pass doas
-\ ufw dhcpcd wpa_supplicant
-\ fzf rxvt-unicode neovim rust clangd black github-cli mcfly
-\ mpv qutebrowser cmus tty-clock # system, dev, and applications
-layman -a guru # many programs(zoxide)
-layman -a vaacus # jedi-language-server
+emerge exa zsh xorg-server alsa-utils htop layman pass doas ufw dhcpcd wpa_supplicant
+\ fzf rxvt-unicode neovim rust ccls github-cli mcfly
+\ mpv sxiv qutebrowser cmus tty-clock # system, dev programs, and programs
+layman -a guru # many programs(zoxide, glow, pfetch, tokei)
 layman -a jjakob # gomuks
-layman -a robertgzr # shfmt
-layman -a ambasta # stylua
-emerge pfetch zoxide bash-language-server lua-language-server gomuks StyLua
+emerge pfetch zoxide gomuks glow tokei
 cd $CONFIG_DIR/xorg/wm/dmenu && make install
 cd ../dwm && make install
 cd ../slstatus && make install
